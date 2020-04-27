ed5bf021182ff7f5e80788463e1ba72087c5482a
github username <github email address>
2020-04-27 23:32:15 +0100

    it works, but not on twitter

--- findText.js
+++ findText.js
@@ -1,23 +1,28 @@
+const button = document.getElementById('change')
 var word = "Bolsonaro",
     queue = [document.body],
     curr
 ;
-while (curr = queue.pop()) {
-    if (!curr.textContent.match(word)) continue;
-    for (var i = 0; i < curr.childNodes.length; ++i) {
-        switch (curr.childNodes[i].nodeType) {
-            case Node.TEXT_NODE : // 3
-                if (curr.childNodes[i].textContent.match(word)) {
-                    console.log("Found!");
-                    var str = curr.childNodes[i].textContent;
 
-curr.childNodes[i].textContent = str.replace(word,'RENATA')
-                    // you might want to end your search here.
-                }
-                break;
-            case Node.ELEMENT_NODE : // 1
-                queue.push(curr.childNodes[i]);
-                break;
+const change = false;
+
+   
+    while (curr = queue.pop()) {
+        if (!curr.textContent.match(word)) continue;
+        for (var i = 0; i < curr.childNodes.length; ++i) {
+            switch (curr.childNodes[i].nodeType) {
+                case Node.TEXT_NODE : // 3
+                    if (curr.childNodes[i].textContent.match(word)) {
+
+                        var str = curr.childNodes[i].textContent;
+
+    curr.childNodes[i].textContent = str.replace(word,'Bostanaro')
+                        // you might want to end your search here.
+                    }
+                    break;
+                case Node.ELEMENT_NODE : // 1
+                    queue.push(curr.childNodes[i]);
+                    break;
+            }
         }
     }
-}
