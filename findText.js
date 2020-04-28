11560f90835aa6f392560f59abea5069e473df4e
github username <github email address>
2020-04-28 10:48:44 +0100

    changed png name

--- findText.js
+++ findText.js
@@ -1,31 +1,28 @@
-const button = document.getElementById('change')
-var word = "Bolsonaro",
+var word = ["Coronavirus","coronavirus","corona"],
     queue = [document.body],
     curr
 ;
 
-const change = false;
-
-chrome.storage.sync.get('color', function(data) {
-   console.log('hello')
-  });
    
-    while (curr = queue.pop()) {
-        if (!curr.textContent.match(word)) continue;
+while (curr = queue.pop()) {
+    for(var j = 0; j < word.length; j++) {
+        if (!curr.textContent.match(word[j])) continue;
         for (var i = 0; i < curr.childNodes.length; ++i) {
             switch (curr.childNodes[i].nodeType) {
                 case Node.TEXT_NODE : // 3
-                    if (curr.childNodes[i].textContent.match(word)) {
-
+                   
+                    if (curr.childNodes[i].textContent.match(word[j])) {
+                    
                         var str = curr.childNodes[i].textContent;
 
-    curr.childNodes[i].textContent = str.replace(word,'Bostanaro')
+                     curr.childNodes[i].textContent = str.replace(word[j],'Flowers')
                         // you might want to end your search here.
                     }
                     break;
                 case Node.ELEMENT_NODE : // 1
                     queue.push(curr.childNodes[i]);
                     break;
-            }
+
+            }}
         }
     }
